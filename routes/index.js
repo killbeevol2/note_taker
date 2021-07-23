b2771719448f2d70c8fef3eb621bd24335b557aa
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-07-23 01:13:22 -0700

    finished all routes and made delete functionable

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,36 @@
 const path = require("path");
+let notes = require("../db/db.json");
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
 
 module.exports = (app) => {
   // api routes
-  app.get("/api/notes", (req, res) => {});
-  app.post("/api/notes", (req, res) => {});
-  app.delete("/api/notes/:id", (req, res) => {});
+  app.get("/api/notes", (req, res) => {
+    console.log(notes);
+    res.json(notes);
+  });
+  app.post("/api/notes", (req, res) => {
+    const note = req.body;
+    note.id = uuid();
+    notes.push(note);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json("New note!");
+    });
+  });
+  app.delete("/api/notes/:id", (req, res) => {
+    notes = notes.filter((note) => note.id !== req.params.id);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      if (err) throw err;
+      res.json("Note deleted!");
+    });
+  });
   // html routes
   app.use("/notes", (req, res) => {
     console.log("/notes");
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
-  app.use("/", (req, res) => {
+  app.use("*", (req, res) => {
     console.log("/");
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes")(app);
